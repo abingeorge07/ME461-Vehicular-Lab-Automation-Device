5d23e78dc27a27059232485ba9f31418cd48bc8b
abingeorge07 <66330225+abingeorge07@users.noreply.github.com>
2023-04-21 17:36:45 -0400

    Update

---
+++ truckMain/main/i2c_lib.c
@@ -0,0 +1,345 @@
+#include <arpa/inet.h>
+#include "driver/mcpwm_prelude.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include <stdio.h>
+#include "esp_log.h"
+#include "esp_vfs_dev.h"
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+#include <math.h>
+#include <stdlib.h>
+//GPIO task
+#include "driver/i2c.h"
+// /// ADC
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+#include "driver/pulse_cnt.h"
+#include "driver/gptimer.h"
+#include "esp_attr.h"
+#include "accel_reg.h"
+
+
+
+//////////// Def of I2C
+#define WRITE_BIT                          0x0 // i2c master write
+#define READ_BIT                           0x1  // i2c master read
+#define I2C_EXAMPLE_MASTER_SCL_IO          22   // gpio number for i2c clk
+#define I2C_EXAMPLE_MASTER_SDA_IO          21   // gpio number for i2c data
+#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_0  // i2c port
+#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_FREQ_HZ         400000     // i2c master clock freq
+#define ACK_CHECK_EN                       0x1
+#define ACC_ADDRESS                        0x68
+
+#define MPU6050_REG_DEVID               (0x00) 
+#define ACK_CHECK_DIS                       0x0
+
+#define NUM_CALIBRATION 200.0 // must be greater than NUM_BAD_VALUES
+#define NUM_BAD_VALUES 100.0
+
+
+
+
+
+
+//////////// OFFSETS FOR ACCEL VALUES
+float accel_offset[3] = {0,0,0};
+float gyro_offset[3] = {0,0,0};
+
+// main global variables
+float accel[3];
+float gyro[3];
+float gyro_prev[3] = {0, 0, 0};
+
+// checks if address x is an i2c device
+int testConnection(uint8_t devAddr, int32_t timeout) {
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, (devAddr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+  i2c_master_stop(cmd);
+  int err = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+  i2c_cmd_link_delete(cmd);
+  return err;
+}
+
+
+
+static void i2c_master_init(){
+  // Debug
+  printf("\n>> i2c Config\n");
+  int err;
+
+  // Port configuration
+  int i2c_master_port = I2C_EXAMPLE_MASTER_NUM;
+
+  /// Define I2C configurations
+  i2c_config_t conf;
+  conf.mode = I2C_MODE_MASTER;                              // Master mode
+  conf.sda_io_num = I2C_EXAMPLE_MASTER_SDA_IO;              // Default SDA pin
+  conf.sda_pullup_en = GPIO_PULLUP_ENABLE;                  // Internal pullup
+  conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;              // Default SCL pin
+  conf.scl_pullup_en = GPIO_PULLUP_ENABLE;                  // Internal pullup
+  conf.master.clk_speed = I2C_EXAMPLE_MASTER_FREQ_HZ;       // CLK frequency
+  conf.clk_flags = 0;
+  err = i2c_param_config(i2c_master_port, &conf);           // Configure
+  if (err == ESP_OK) {printf("- parameters: ok\n");}
+
+  // Install I2C driver
+  err = i2c_driver_install(i2c_master_port, conf.mode,
+                     I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
+                     I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
+  if (err == ESP_OK) {printf("- initialized: yes\n");}
+
+  // Data in MSB mode
+  i2c_set_data_mode(i2c_master_port, I2C_DATA_MODE_MSB_FIRST, I2C_DATA_MODE_MSB_FIRST);
+}
+
+
+
+// Get Device ID
+int getDeviceID(uint8_t *data) {
+  int ret;
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, (ACC_ADDRESS << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd, MPU6050_REG_DEVID, ACK_CHECK_EN);
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, (ACC_ADDRESS << 1 ) | READ_BIT, ACK_CHECK_EN);
+  i2c_master_read_byte(cmd, data, ACK_CHECK_DIS);
+  i2c_master_stop(cmd);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+  i2c_cmd_link_delete(cmd);
+  return ret;
+}
+
+
+// Utility function to scan for i2c device
+// Returns the count of I2C devices
+static uint8_t i2c_scanner() {
+  int32_t scanTimeout = 1000;
+  printf("\n>> I2C scanning ..."  "\n");
+  uint8_t count = 0;
+  for (uint8_t i = 1; i < 127; i++) {
+    if (testConnection(i, scanTimeout) == ESP_OK) {
+      printf( "- Device found at address: 0x%X%s", i, "\n");
+      count++;
+    }
+  }
+  if (count == 0) {printf("- No I2C devices found!" "\n");}
+
+  return count;
+}
+
+
+// writing one byte to a register
+void writeRegister(uint8_t reg, uint8_t data, uint8_t address) {
+
+    // start
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+
+    // Decvice# + read or write
+    i2c_master_write_byte(cmd, (address << 1) | WRITE_BIT, I2C_MASTER_ACK);
+    // Reg reading or writing from
+    i2c_master_write_byte(cmd, reg, I2C_MASTER_ACK);
+    // Data to write to or store from slave
+    i2c_master_write_byte(cmd, data, I2C_MASTER_ACK);
+
+    //stoop
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+}
+
+// reads register
+uint8_t readRegister(uint8_t reg , uint8_t address) {
+
+
+  uint8_t data1; //first byte MSB
+
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_cmd_handle_t cmd1 = i2c_cmd_link_create();
+
+  // Start
+  i2c_master_start(cmd);
+  // Master write slave address + write bit
+  i2c_master_write_byte(cmd, ( address << 1 ) | WRITE_BIT, I2C_MASTER_ACK); 
+  // Master write register address + send ack
+  i2c_master_write_byte(cmd, reg, I2C_MASTER_ACK); 
+  //master stops
+  i2c_master_stop(cmd);
+  // This starts the I2C communication
+  i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS); 
+  i2c_cmd_link_delete(cmd);
+
+  //master starts
+  i2c_master_start(cmd1);
+  // Master write slave address + read bit
+  i2c_master_write_byte(cmd1, ( address  << 1 ) | READ_BIT, I2C_MASTER_ACK);  
+  // Master reads in slave ack and data
+  i2c_master_read_byte(cmd1, &data1 , I2C_MASTER_NACK);
+  // Master nacks and stops.
+  i2c_master_stop(cmd1);
+  // This starts the I2C communication
+  i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd1, 1000 / portTICK_PERIOD_MS); 
+  i2c_cmd_link_delete(cmd1);
+
+  return data1;
+}
+
+// sets range for gyro
+void setGyroRange(uint8_t sel){
+
+    /* Red the data format register to preserve bits */
+    uint8_t format = readRegister(GYRO_RANGE, ACC_ADDRESS);
+
+    // bit manipulation
+    format &=  0xE7;
+    format = format | (sel<<3);
+    
+    // write back to the register
+    writeRegister(GYRO_RANGE, format, ACC_ADDRESS);
+}
+
+
+// sets range for gyro
+void setAccelRange(uint8_t sel){
+
+    /* Red the data format register to preserve bits */
+    uint8_t format = readRegister(ACC_RANGE, ACC_ADDRESS);
+
+    // bit manipulation
+    format &=  0xE7;
+    format = format | (sel<<3);
+    
+    // write back to the register
+    writeRegister(ACC_RANGE, format, ACC_ADDRESS);
+}
+
+// read 16 bits (reg and reg+1)
+int16_t read16(uint8_t reg , uint8_t address){
+    
+    uint8_t MSB = readRegister(reg, ACC_ADDRESS); 
+    uint8_t LSB = readRegister(reg+1, ACC_ADDRESS); 
+
+    int16_t  out = (int16_t) ((MSB << 8) | LSB);
+
+    return out;
+}
+
+void getAccel(float* accel){
+
+    accel[0] = read16(REG_AX_H, ACC_ADDRESS)/16384.0 - accel_offset[0];// - (2.0*9.81);
+    // accel[0] = (float) val;
+
+    accel[1] = read16(REG_AY_H, ACC_ADDRESS)/16384.0  - accel_offset[1];// - (2.0*9.81);
+    // accel[1] = (float) val;
+
+    accel[2] = read16(REG_AZ_H, ACC_ADDRESS)/16384.0  - accel_offset[2];// - (2.0*9.81);
+    // accel[2] = (float) val;
+
+}
+
+
+void getGyro(float* gyro){
+
+    float val = read16(REG_GX_H, ACC_ADDRESS) /131.0;
+    gyro[0] = (float) val - gyro_offset[0];
+
+    val = read16(REG_GY_H, ACC_ADDRESS) /131.0;
+    gyro[1] = (float) val - gyro_offset[1];
+
+    val = read16(REG_GZ_H, ACC_ADDRESS) /131.0;
+    gyro[2] = (float) val - gyro_offset[2];
+
+}
+
+
+void calibration(float* accel, float* gyro){
+
+  float accelOffset[3];
+  float gyroOffset[3];
+  int i;
+
+  printf("-------------CALIBRATION--------------\n");
+
+  for(i=0; i<NUM_CALIBRATION; i++){
+    getAccel(accel);
+    getGyro(gyro);
+  
+    if(i>=NUM_BAD_VALUES){
+      accelOffset[0] +=  accel[0];
+      accelOffset[1] +=  accel[1];
+      accelOffset[2] +=  (accel[2] + 9.8);
+
+      gyroOffset[0] += gyro[0];
+      gyroOffset[1] += gyro[1];
+      gyroOffset[2] += gyro[2];
+    }
+
+    vTaskDelay(50 / portTICK_PERIOD_MS);   
+
+  }
+
+  printf("-------------END CALIBRATION--------------\n");
+
+  for (i=0; i<3; i++){
+    accel_offset[i] = accelOffset[i] / (NUM_CALIBRATION-NUM_BAD_VALUES);
+    gyro_offset[i] =  gyroOffset[i] / (NUM_CALIBRATION-NUM_BAD_VALUES);
+  }
+
+  printf("Offset Values :\nAccel: %f %f %f\n", accel_offset[0], accel_offset[1], accel_offset[2]);
+  printf("Gyro: %f %f %f\n", gyro_offset[0], gyro_offset[1], gyro_offset[2]);
+
+}
+
+
+void init_IMU(){
+  // IMU initializations
+    i2c_master_init();
+    i2c_scanner();
+
+    // UART Init
+    ESP_ERROR_CHECK( uart_driver_install(UART_NUM_0,
+    256, 0, 0, NULL, 0) );
+
+    /* Tell VFS to use UART driver */
+    esp_vfs_dev_uart_use_driver(UART_NUM_0);
+
+    uint8_t deviceID_acc;
+    getDeviceID(&deviceID_acc);
+
+    // Disable interrupts
+    writeRegister(REG_INTR_EN, 0, ACC_ADDRESS);
+
+    // setRange
+    setGyroRange(GYRO250);
+
+    // // setRange
+    setAccelRange(ACC250);
+
+    //turn off standby mode or start measuring
+    writeRegister(0x6B, 0, ACC_ADDRESS);
+
+    // calibration of IMU
+    calibration(accel, gyro);
+}
+
+
---
+++ truckMain/main/truck.c
@@ -0,0 +1,420 @@
+#include <string.h>
+#include <stdio.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+#include "i2c_lib.c"
+#include <rom/ets_sys.h>
+#include "IR_lib.c"
+#include "motor_lib.c"
+#include "encoder_lib.c"
+
+// MOTOR WIRING
+// PWM ----------> PIN 32
+// DIR 1---------> PIN 25
+// DIR 2---------> PIN 33
+
+
+// Encoder
+// Signal 1 ------> PIN 14
+// Signal 2 ------> PIN 27
+
+#define PORT CONFIG_EXAMPLE_PORT
+#define MINIMUM_MOTOR_PWM 5000
+#define STOP_MOTOR_PWM 3000
+#define THRESHOLD_IR 4000
+#define RADIUS 1.0
+#define DT_ENCODER 50
+#define DT_MOTOR 50
+#define INIT_PWM 5000
+#define DT_MOTOR 50
+#define ACC_ADD 0x68 // address of IMU
+#define STEADY_SPEED_INCREASE 6000
+
+#define MAX_PWM 35000
+#define MIN_PWN 5000
+
+// MOTOR PINS
+#define GPIO_PWM0A_OUT 25   //Used to control direction
+#define GPIO_PWM0B_OUT 33   //Used to control direction
+#define PWM_MOTOR_GPIO  32  //Set PWM for speed
+
+// global variables
+struct station{
+    int stationNum;
+    float position;
+    float absPosition; // this is what the encoder reads
+};
+
+struct station* stationArr = NULL;
+int numberStations = 0;
+int currentStation = 0;
+int startMotor = 0;
+float error = 0;
+float derror_dt = 0;
+float serror_dt = 0;
+float prevError = 0;
+float change = 0;
+float distance2move = 0;
+int pwm = 0;
+int calStarted = 0;
+
+
+// PID constants
+float K_P = 5;
+float K_I = 0.1;
+float K_D = -3000;
+
+void startCal(){
+
+    if(stationArr != NULL)
+        free(stationArr);
+    
+    stationArr = (struct station*) malloc(sizeof(struct station)*numberStations);
+    
+
+    // beginning the calibration
+    brushed_motor_forward();
+    motor_start();
+    motor_setSpeed(MINIMUM_MOTOR_PWM);
+
+    uint32_t IR_reading = 0;
+
+    while(1){
+        IR_reading= getIR();
+        printf("IR = %ld\n", IR_reading);
+        
+        if(IR_reading>THRESHOLD_IR){
+            // stop the truck
+            motor_setSpeed(STOP_MOTOR_PWM);
+            brushed_motor_stop();
+
+            // reached the end
+            if(currentStation == numberStations){
+                calStarted = 0;
+                currentStation = 0;
+                vTaskDelete(NULL);
+                break;
+            }
+            stationArr[currentStation].stationNum = currentStation+1;
+            // add line to store the distance information
+            stationArr[currentStation].absPosition = get_absolutePosition();
+
+            // first station is position 0
+            if(currentStation == 0)
+            {
+                stationArr[currentStation].position = 0;
+            }
+            else{
+                stationArr[currentStation].position = get_absolutePosition() - stationArr[currentStation-1].absPosition;
+            }
+            currentStation++;
+            vTaskDelay(2000/portTICK_PERIOD_MS);
+            brushed_motor_forward();
+            motor_setSpeed(MINIMUM_MOTOR_PWM);
+            vTaskDelay(1000/portTICK_PERIOD_MS);
+        }
+            
+
+        vTaskDelay(30/portTICK_PERIOD_MS);
+    }
+}
+
+
+
+void motor_task(void){
+
+    while(1){
+        if(startMotor == 1){
+            if(error > 0){
+                brushed_motor_forward();
+            }
+            else{
+                brushed_motor_backward();
+            }
+
+            if(distance2move <= 15 || distance2move >= -15){
+                pwm = STEADY_SPEED_INCREASE - INIT_PWM;
+                motor_setSpeed(STEADY_SPEED_INCREASE);
+            }
+            else{
+                if(error>0 && change >0){
+                    pwm = (change/distance2move) * 2000;
+                    
+                    if((pwm+INIT_PWM) >= MAX_PWM)
+                        pwm = MAX_PWM - 1 - INIT_PWM;
+                    else if((pwm+INIT_PWM) <= MIN_PWN)
+                        pwm = 0;
+
+                    motor_setSpeed(INIT_PWM + pwm);
+                }
+                else if (error > 0 && change< 0){
+                    pwm = (change/distance2move) * 2000;
+                    if((pwm+INIT_PWM) >= MAX_PWM)
+                        pwm = MAX_PWM - 1 - INIT_PWM;
+                    else if((pwm+INIT_PWM) <= MIN_PWN)
+                        pwm = 0;
+
+                    motor_setSpeed(INIT_PWM + pwm);
+                }
+                else if (error < 0 && change < 0){
+                    pwm = (change/distance2move) * 2000;
+
+                    if((pwm+INIT_PWM) >= MAX_PWM)
+                        pwm = MAX_PWM - 1 - INIT_PWM;
+                    else if((pwm+INIT_PWM) <= MIN_PWN)
+                        pwm = 0;
+
+
+                    motor_setSpeed(INIT_PWM + pwm);
+                }
+                else if (error < 0 && change > 0 ){
+                    pwm = (change/distance2move) * 2000;
+
+                    if((pwm+INIT_PWM) >= MAX_PWM)
+                        pwm = MAX_PWM - 1 - INIT_PWM;
+                    else if((pwm+INIT_PWM) <= MIN_PWN)
+                        pwm = MIN_PWN - INIT_PWM;
+
+
+                    motor_setSpeed(INIT_PWM + pwm);
+                }
+
+            }
+        }
+        else{
+            brushed_motor_stop();
+            vTaskDelete(NULL);
+        }
+
+        vTaskDelay(DT_MOTOR/ portTICK_PERIOD_MS);
+    }
+}
+
+
+
+
+
+
+void PID_task(int nextStation){
+    distance2move = stationArr[nextStation-1].position - stationArr[currentStation].position;
+    float finalPosition = get_absolutePosition() + distance2move;
+    startMotor = 1;
+
+    while(1){
+        error = finalPosition - get_absolutePosition();
+        derror_dt = (error - prevError)/ (DT_ENCODER);
+        serror_dt = serror_dt + (((error+prevError)/2)*DT_ENCODER/1000.0);
+
+        prevError = error;
+        change = ((error * K_P) + (derror_dt * K_D) + (serror_dt*K_I));
+
+        if(error<.1 && error>-.1){
+            startMotor = 0;
+            brushed_motor_stop();
+            serror_dt = 0;
+            break;
+        }
+
+        xTaskCreate(motor_task, "motor_task", 4096, NULL, 5, NULL);
+        vTaskDelay(DT_ENCODER/ portTICK_PERIOD_MS);
+    }
+}
+
+static void udp_server_task(void *pvParameters)
+{
+    char rx_buffer[128];
+    char addr_str[128];
+    int addr_family = (int)pvParameters;
+    int ip_protocol = 0;
+    struct sockaddr_in6 dest_addr;
+
+    while (1) {
+
+        if (addr_family == AF_INET) {
+            struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
+            dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+            dest_addr_ip4->sin_family = AF_INET;
+            dest_addr_ip4->sin_port = htons(PORT);
+            ip_protocol = IPPROTO_IP;
+        } else if (addr_family == AF_INET6) {
+            bzero(&dest_addr.sin6_addr.un, sizeof(dest_addr.sin6_addr.un));
+            dest_addr.sin6_family = AF_INET6;
+            dest_addr.sin6_port = htons(PORT);
+            ip_protocol = IPPROTO_IPV6;
+        }
+
+        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created");
+
+#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
+        int enable = 1;
+        lwip_setsockopt(sock, IPPROTO_IP, IP_PKTINFO, &enable, sizeof(enable));
+#endif
+
+#if defined(CONFIG_EXAMPLE_IPV4) && defined(CONFIG_EXAMPLE_IPV6)
+        if (addr_family == AF_INET6) {
+            // Note that by default IPV6 binds to both protocols, it is must be disabled
+            // if both protocols used at the same time (used in CI)
+            int opt = 1;
+            setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+            setsockopt(sock, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof(opt));
+        }
+#endif
+        // Set timeout
+        struct timeval timeout;
+        timeout.tv_sec = 10;
+        timeout.tv_usec = 0;
+        setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
+
+        int err = bind(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+        if (err < 0) {
+            ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        }
+        ESP_LOGI(TAG, "Socket bound, port %d", PORT);
+
+        struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+        socklen_t socklen = sizeof(source_addr);
+
+#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
+        struct iovec iov;
+        struct msghdr msg;
+        struct cmsghdr *cmsgtmp;
+        u8_t cmsg_buf[CMSG_SPACE(sizeof(struct in_pktinfo))];
+
+        iov.iov_base = rx_buffer;
+        iov.iov_len = sizeof(rx_buffer);
+        msg.msg_control = cmsg_buf;
+        msg.msg_controllen = sizeof(cmsg_buf);
+        msg.msg_flags = 0;
+        msg.msg_iov = &iov;
+        msg.msg_iovlen = 1;
+        msg.msg_name = (struct sockaddr *)&source_addr;
+        msg.msg_namelen = socklen;
+#endif
+
+        while (1) {
+            // ESP_LOGI(TAG, "Waiting for data");
+#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
+            int len = recvmsg(sock, &msg, 0);
+#else
+            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+#endif
+            // Error occurred during receiving
+            if (len < 0) {
+                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+                break;
+            }
+            // Data received
+            else {
+                // Get the sender's ip address as string
+                if (source_addr.ss_family == PF_INET) {
+                    inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+                    #if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
+                    for ( cmsgtmp = CMSG_FIRSTHDR(&msg); cmsgtmp != NULL; cmsgtmp = CMSG_NXTHDR(&msg, cmsgtmp) ) {
+                        if ( cmsgtmp->cmsg_level == IPPROTO_IP && cmsgtmp->cmsg_type == IP_PKTINFO ) {
+                            struct in_pktinfo *pktinfo;
+                            pktinfo = (struct in_pktinfo*)CMSG_DATA(cmsgtmp);
+                            ESP_LOGI(TAG, "dest ip: %s\n", inet_ntoa(pktinfo->ipi_addr));
+                        }
+                    }       
+                    #endif
+                } else if (source_addr.ss_family == PF_INET6) {
+                    inet6_ntoa_r(((struct sockaddr_in6 *)&source_addr)->sin6_addr, addr_str, sizeof(addr_str) - 1);
+                }
+
+                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
+                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+                ESP_LOGI(TAG, "%s", rx_buffer);
+                
+                char text[100];
+                // start calibration
+                if(strncmp(rx_buffer, "calXX", 2) == 0){
+                    numberStations = atoi(rx_buffer+4);
+                    printf("Number of Stations is %d\n", numberStations);
+                    xTaskCreate(startCal, "startCal", 4096, NULL, 5, NULL); 
+                    calStarted = 1;
+                    sprintf(text, "OK");
+                }
+                // listening
+                else if(strncmp(rx_buffer, "Listening", 6) == 0){
+
+                    if(calStarted == 1){
+                        sprintf(text, "OK");
+                        printf("calStarted %d\n", calStarted);
+                        // sprintf(text,"Ax: %f, Ay: %f, Az:%f, Gx: %f, Gy:%f, Gz:%f\n", accel[0], accel[1], accel[2], gyro[0], gyro[1], gyro[2]);
+                    }
+                    else{
+                        printf("Reached here\n");
+                        sprintf(text, "Done\n");
+                    }
+
+                }
+                else if(strncmp(rx_buffer, "move2", 5) == 0){
+                    int nextStation = atoi(rx_buffer + 6);
+                    PID_task(nextStation);
+                }
+                int err = sendto(sock, text, strlen(text), 0, (struct sockaddr *)&source_addr, sizeof(source_addr));
+                if (err < 0) {
+                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                    break;
+                }
+            }
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+
+    ESP_ERROR_CHECK(example_connect());
+
+
+    //motor init
+    motor_init();
+    brushed_motor_stop();
+    // IR init
+    IR_init();
+
+    //encoder init
+    encoderInit();
+    setRadius(RADIUS);
+
+    #ifdef CONFIG_EXAMPLE_IPV4
+        xTaskCreate(udp_server_task, "udp_server", 4096, (void*)AF_INET, 5, NULL);
+    #endif
+    #ifdef CONFIG_EXAMPLE_IPV6
+        xTaskCreate(udp_server_task, "udp_server", 4096, (void*)AF_INET6, 5, NULL);
+    #endif
+
+}
+
+
